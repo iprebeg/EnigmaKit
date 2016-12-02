45dae08173513ad3d9bc63c50b760d84f5a9d118
Ivor Prebeg <ivor.prebeg@gmail.com>
2016-12-02 12:05:53 +0000

    Initial Commit

---
+++ EnigmaKit/EnigmaKit.h
@@ -0,0 +1,19 @@
+//
+//  EnigmaKit.h
+//  EnigmaKit
+//
+//  Created by Prebeg, Ivor on 02/12/2016.
+//  Copyright © 2016 Prebeg, Ivor. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for EnigmaKit.
+FOUNDATION_EXPORT double EnigmaKitVersionNumber;
+
+//! Project version string for EnigmaKit.
+FOUNDATION_EXPORT const unsigned char EnigmaKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EnigmaKit/PublicHeader.h>
+
+
